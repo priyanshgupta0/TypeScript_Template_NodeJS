7ca36464830a47b18efa3c2d3dbb382a91f7fe28
priyanshgupta0 <pg167783@gmail.com>
2025-06-05 20:40:00 +0530

    refactor: remove compiled distribution files from the repository

--- dist/src/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res, next) => {
-    const isAuthenticated = true; // Replace with actual logic
-    if (!isAuthenticated)
-        return res.status(401).json({ error: "Unauthorized" });
-    next();
-};
